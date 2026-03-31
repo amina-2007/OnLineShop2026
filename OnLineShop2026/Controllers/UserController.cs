74c58dba1bae3856759380a2e527da3cbd348978
amina-2007 <aminagatagova@mail.ru>
2026-03-31 18:26:39 +0300

    done

---
+++ OnLineShop2026/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnLineShop2026.Models;
+
+namespace OnLineShop2026.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult UserView()
+        {
+            User user = new User
+            (
+              Guid.NewGuid(),
+               "AMINA",
+              "2007",
+               "example@gmail.com",
+                "/images/img_cake.jpg"
+            );
+            return View(user);
+        }
+
+    }
+}
---
+++ OnLineShop2026/Models/User.cs
@@ -0,0 +1,21 @@
+﻿namespace OnLineShop2026.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PFPPath { get; set; } = "/images/sausage.jpg";
+
+        public User(Guid id, string name, string lastName, string email, string pFPPath)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastName;
+            Email = email;
+            PFPPath = pFPPath;
+        }
+    }
+
+}

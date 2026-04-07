1ab6fa0ddf4fa1833823706e59eabdaf2593aeff
amina-2007 <aminagatagova@mail.ru>
2026-04-07 19:11:09 +0300

    gatagova_lesson_3_1 sozdala userrrepository

---
+++ OnLineShop2026/Data/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using OnLineShop2026.Models;
+
+namespace OnLineShop2026.Data
+{
+    public class UserRepository
+    {
+        List<User> users=new List<User>();
+
+        public List<User> GetAll()
+        {
+            return users;
+        }
+
+        public User? TryGetById(Guid id)
+        {
+            return users.FirstOrDefault(product => product.Id == id);
+        }
+    }
+}

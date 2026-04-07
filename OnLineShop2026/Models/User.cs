4929e09311e4fd5d292462444e0ab8a544738e5e
amina-2007 <aminagatagova@mail.ru>
2026-04-07 18:54:28 +0300

    gatagova_lesson_2_1

--- OnLineShop2026/Models/User.cs
+++ OnLineShop2026/Models/User.cs
@@ -1,7 +1,7 @@
 ﻿namespace OnLineShop2026.Models
 {
     public class User
-    {
+    { 
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }

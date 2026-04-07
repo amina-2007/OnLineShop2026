8bd0f4a348af73d5ce8a2b2a6a290e10a15ce0a3
amina-2007 <aminagatagova@mail.ru>
2026-04-07 19:15:44 +0300

    gatagova_lesson_3_2

--- OnLineShop2026/Controllers/UserController.cs
+++ OnLineShop2026/Controllers/UserController.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OnLineShop2026.Models;
-
+using OnLineShop2026.Data;
 namespace OnLineShop2026.Controllers
 {
     public class UserController : Controller 
     {
-        public IActionResult UserView()
+      /*  public IActionResult UserView()
         {
-            User user = new User
+           User user = new User
             (
               Guid.NewGuid(),
                "AMINA",
@@ -17,6 +17,12 @@ namespace OnLineShop2026.Controllers
             );
             return View(user);
         }
-
+      */
+      private UserRepository userRepository=new UserRepository();   
+      public IActionResult Index()
+        {
+            var users=userRepository.GetAll();
+            return View(users);
+        }
     }
 }
